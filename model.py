40bbf917d344b8e1a38f2437f07aa109bf2a0e6c
Grigorii Antiokh <grygoryant@gmail.com>
2018-01-11 15:04:27 +0000

    Adding generator

--- model.py
+++ model.py
@@ -1,27 +1,46 @@
+from random import shuffle
 import csv
 import cv2
 import numpy as np
 
 lines = []
-with open('./data/data/driving_log.csv') as data_file:
+with open('./data/driving_log.csv') as data_file:
 	reader = csv.reader(data_file)
 	next(reader, None)
 	for line in reader:
 		lines.append(line)
 
-images = []
-measurements = []
-for line in lines:
-	source_path = line[0]
-	measurement = float(line[3])
-	file_name = source_path.split('/')[-1]
-	current_path = './data/data/IMG/' + file_name
-	image = cv2.imread(current_path)
-	images.append(image)
-	measurements.append(measurement)
+from sklearn.model_selection import train_test_split
+train_samples, validation_samples = train_test_split(lines, test_size=0.2)
 
-X_train = np.array(images)
-y_train = np.array(measurements)
+import sklearn
+
+def generator(samples, batch_size=32):
+	num_samples = len(samples)
+	while 1:
+		shuffle(samples)
+		for offset in range(0, num_samples, batch_size):
+			batch_samples = samples[offset:offset+batch_size]
+
+			images = []
+			angles = []
+			for batch_sample in batch_samples:
+				center_name = './data/IMG/'+batch_sample[0].split('/')[-1]
+				center_image = cv2.imread(center_name)
+				center_image_mirrored = cv2.flip(center_image, 0)
+				center_angle = float(batch_sample[3])
+				center_angle_mirrored = -center_angle
+				images.append(center_image)
+				angles.append(center_angle)
+				images.append(center_image_mirrored)
+				angles.append(center_angle_mirrored)
+
+			X_train = np.array(images)
+			y_train = np.array(angles)
+			yield sklearn.utils.shuffle(X_train, y_train)
+
+train_generator = generator(train_samples, batch_size=32)
+validation_generator = generator(validation_samples, batch_size=32)
 
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda, Cropping2D
@@ -29,7 +48,7 @@ from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
 
 model = Sequential()
-model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
+model.add(Lambda(lambda x: x / 127.5 - 1., input_shape=(160, 320, 3)))
 model.add(Cropping2D(cropping=((70, 25), (0, 0))))
 model.add(Convolution2D(24,5,5,subsample=(2,2),activation="relu"))
 model.add(Convolution2D(36,5,5,subsample=(2,2),activation="relu"))
@@ -43,6 +62,6 @@ model.add(Dense(10))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=10)
+model.fit_generator(train_generator, samples_per_epoch=len(train_samples), validation_data=validation_generator, nb_val_samples=len(validation_samples), nb_epoch=10)
 
 model.save('model.h5')
