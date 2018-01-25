ee9742ce8d2f6a773bb368f577aab04b618eda7e
Grigorii Antiokh <grygoryant@gmail.com>
2018-01-25 03:59:02 +0000

    Dropouts added

--- model.py
+++ model.py
@@ -63,22 +63,33 @@ train_generator = generator(train_samples, batch_size=32)
 validation_generator = generator(validation_samples, batch_size=32)
 
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda, Cropping2D
+from keras.layers import Flatten, Dense, Lambda, Cropping2D, Dropout
 from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
 
 model = Sequential()
 model.add(Lambda(lambda x: x / 127.5 - 1., input_shape=(160, 320, 3)))
 model.add(Cropping2D(cropping=((70, 25), (0, 0))))
+model.add(Dropout(0.2))
 model.add(Convolution2D(24,5,5,subsample=(2,2),activation="relu"))
+model.add(Dropout(0.2))
 model.add(Convolution2D(36,5,5,subsample=(2,2),activation="relu"))
+model.add(Dropout(0.2))
 model.add(Convolution2D(48,5,5,subsample=(2,2),activation="relu"))
+model.add(Dropout(0.2))
 model.add(Convolution2D(64,3,3,activation="relu"))
+model.add(Dropout(0.2))
 model.add(Convolution2D(64,3,3,activation="relu"))
+
+#model.add(Dropout(0.2))
 model.add(Flatten())
+#model.add(Dropout(0.2))
 model.add(Dense(100))
+#model.add(Dropout(0.2))
 model.add(Dense(50))
+#model.add(Dropout(0.2))
 model.add(Dense(10))
+#model.add(Dropout(0.2))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
