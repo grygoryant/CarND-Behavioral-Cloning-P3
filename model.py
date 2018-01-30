6b8ccd470b5bd6f90e7ef760fd4b82bb1025b5d3
Grigorii Antiokh <grygoryant@gmail.com>
2018-01-30 02:57:13 +0000

    Outputting model scheme, improved data preparation methodology

--- model.py
+++ model.py
@@ -15,6 +15,12 @@ train_samples, validation_samples = train_test_split(lines, test_size=0.2)
 
 import sklearn
 
+def preprocess_frame(img):
+	ret_img = cv2.GaussianBlur(img, (3, 3), 0)
+	#ret_img = cv2.cvtColor(ret_img, cv2.COLOR_BGR2YUV)
+	return ret_img
+	
+
 def generator(samples, batch_size=32):
 	num_samples = len(samples)
 	while 1:
@@ -34,26 +40,26 @@ def generator(samples, batch_size=32):
 				center_angle = float(batch_sample[3])
 				left_angle = center_angle + 0.2
 				right_angle = center_angle - 0.2
-				images.append(center_image)
+				images.append(preprocess_frame(center_image))
 				angles.append(center_angle)
-				images.append(left_image)
+				images.append(preprocess_frame(left_image))
 				angles.append(left_angle)
-				images.append(right_image)
+				images.append(preprocess_frame(right_image))
 				angles.append(right_angle)
-				if abs(center_angle) > 0.1:
+				if abs(center_angle) > 0.3:
 					center_image_mirrored = cv2.flip(center_image, 1)
 					center_angle_mirrored = -center_angle
-					images.append(center_image_mirrored)
+					images.append(preprocess_frame(center_image_mirrored))
 					angles.append(center_angle_mirrored)
-				if abs(left_angle) > 0.1:
+				if abs(left_angle) > 0.3:
 					left_image_mirrored = cv2.flip(left_image, 1)
 					left_angle_mirrored = -left_angle
-					images.append(left_image_mirrored)
+					images.append(preprocess_frame(left_image_mirrored))
 					angles.append(left_angle_mirrored)
-				if abs(right_angle) > 0.1:
+				if abs(right_angle) > 0.3:
 					right_image_mirrored = cv2.flip(right_image, 1)
 					right_angle_mirrored = - right_angle
-					images.append(right_image_mirrored)
+					images.append(preprocess_frame(right_image_mirrored))
 					angles.append(right_angle_mirrored)
 			X_train = np.array(images)
 			y_train = np.array(angles)
@@ -66,31 +72,35 @@ from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda, Cropping2D, Dropout
 from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
+from keras.utils.visualize_util import plot
+from keras.regularizers import l2
 
 model = Sequential()
 model.add(Lambda(lambda x: x / 127.5 - 1., input_shape=(160, 320, 3)))
 model.add(Cropping2D(cropping=((70, 25), (0, 0))))
 model.add(Dropout(0.2))
-model.add(Convolution2D(24,5,5,subsample=(2,2),activation="relu"))
+model.add(Convolution2D(24,5,5,subsample=(2,2),activation="relu"))#, W_regularizer=l2(0.001)))
 model.add(Dropout(0.2))
-model.add(Convolution2D(36,5,5,subsample=(2,2),activation="relu"))
+model.add(Convolution2D(36,5,5,subsample=(2,2),activation="relu"))#, W_regularizer=l2(0.001)))
 model.add(Dropout(0.2))
-model.add(Convolution2D(48,5,5,subsample=(2,2),activation="relu"))
+model.add(Convolution2D(48,5,5,subsample=(2,2),activation="relu"))#, W_regularizer=l2(0.001)))
 model.add(Dropout(0.2))
-model.add(Convolution2D(64,3,3,activation="relu"))
+model.add(Convolution2D(64,3,3,activation="relu"))#, W_regularizer=l2(0.001)))
 model.add(Dropout(0.2))
-model.add(Convolution2D(64,3,3,activation="relu"))
+model.add(Convolution2D(64,3,3,activation="relu"))#, W_regularizer=l2(0.001)))
 
 #model.add(Dropout(0.2))
 model.add(Flatten())
 #model.add(Dropout(0.2))
-model.add(Dense(100))
+model.add(Dense(100))#, W_regularizer=l2(0.001)))
 #model.add(Dropout(0.2))
-model.add(Dense(50))
+model.add(Dense(50))#, W_regularizer=l2(0.001)))
 #model.add(Dropout(0.2))
-model.add(Dense(10))
+model.add(Dense(10))#, W_regularizer=l2(0.001)))
 #model.add(Dropout(0.2))
 model.add(Dense(1))
+
+plot(model, to_file='model.png', show_shapes=True)
 
 model.compile(loss='mse', optimizer='adam')
 model.fit_generator(train_generator, samples_per_epoch=len(train_samples), validation_data=validation_generator, nb_val_samples=len(validation_samples), nb_epoch=10)
